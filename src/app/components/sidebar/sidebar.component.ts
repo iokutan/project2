a73f5ad44ddd81d0599b1ae75bea9b1cfb7b5b9e
Ilyas Okutan <ilyasemin@ilyass-macbook-pro.home>
2020-01-06 19:29:14 +0100

    commit

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
 
   constructor() { }
+  lines: any [] = [
+    { title: 'Auftrag Verfolgen',
+      description: 'Erfahren Sie mehr über Status Ihren Auftrag.',
+      position: '',
+      image: '/assets/images/tracking.jpg'},
+    { title: 'Termin Vereinbaren',
+      description: 'Vermeiden Sie ungewünschte Wartezeiten.',
+      position: 'order-md-2',
+      image: '/assets/images/termin.jpg'}
+  ];
 
   ngOnInit() {
   }
