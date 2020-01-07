3546a553222b038cc222bd94dd69b6b4ab199268
Ilyas Okutan <ilyasemin@Ilyass-MacBook-Pro.local>
2020-01-07 21:38:00 +0100

    commit

--- src/app/services/sale.service.ts
+++ src/app/services/sale.service.ts
@@ -14,11 +14,11 @@ export class SaleService {
 
   public getSaleUhren(): Observable<any> {
     const data =  [
-      { id: 1, image: '/assets/images/goldsmith.jpg', name: 'Rado', description: 'Automatic, Herren Uhr Keramik', price: '890' },
-      { id: 2, image: '/assets/images/goldsmith.jpg', name: 'Tissot', description: 'Quartz, Herren Uhr Keramik', price: '1200' },
-      { id: 3, image: '/assets/images/goldsmith.jpg', name: 'Quartz', description: 'Quartz, Herren Uhr Keramik', price: '450' },
-      { id: 4, image: '/assets/images/goldsmith.jpg', name: 'Sony', description: 'Automatic, Herren Uhr Keramik', price: '1500' },
-      { id: 5, image: '/assets/images/goldsmith.jpg', name: 'Citizen', description: 'Mekanik, Herren Uhr Keramik', price: '350' }
+      { id: 1, image: '/assets/images/rado.jpg', name: 'Rado', model: 'Automatic', price: '2890' },
+      { id: 2, image: '/assets/images/tissot.jpg', name: 'Tissot', model: 'Quartz', price: '1200' },
+      { id: 3, image: '/assets/images/certina.jpg', name: 'Certina', model: 'Quartz', price: '650' },
+      { id: 4, image: '/assets/images/swatch.jpg', name: 'Swatch', model: 'Automatic', price: '150' },
+      { id: 5, image: '/assets/images/seiko.jpg', name: 'Seiko', model: 'Mekanik', price: '350' }
     ];
 
     return of(data);
