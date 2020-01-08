5b744261bad3aed67da9f421f127af227c589894
Ilyas Okutan <ilyasemin@Ilyass-MacBook-Pro.local>
2020-01-09 00:07:42 +0100

    commit

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Feature } from 'src/app/Feature';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'cristal-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
@@ -8,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
 export class SliderComponent implements OnInit {
 
   constructor() { }
+  sliders: Feature[] = [{
+    title: 'Goldschmied und Uhrenmacher vor Ort!',
+    description: 'Ab Januar 2020 ist auch unser Goldschmied für Sie vor Ort da!',
+    link: '/services/service-main',
+    position: 'active',
+    image: '/assets/images/goldsmith.jpg'
+  },
 
+  {
+    title: 'Online Offerte einholen für Ihr Altgold, Markenuhr oder Nachfrage !',
+    description: 'Die Anfragen sind natürlich für Sie absolut unverbindlich und kostenfrei.',
+    link: '/contact',
+    position: '',
+    image: '/assets/images/ankauf-gold2.jpg'
+  },
+
+  {
+    title: 'Valentinstag Aktion',
+    description: ' Geschenk für ihn <=> Geschenk für sie',
+    link: '/aktuel',
+    position: '',
+    image: '/assets/images/valentinstag2.jpg'
+  },
+
+];
   ngOnInit() {
   }
 
