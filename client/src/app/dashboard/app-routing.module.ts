490c2868dcb57e739b7e63553c1ef1033a384b49
Ilyas Okutan <ilyasemin@ilyass-macbook-pro.home>
2020-02-03 17:14:34 +0100

    commit

--- client/src/app/dashboard/app-routing.module.ts
+++ client/src/app/dashboard/app-routing.module.ts
@@ -5,6 +5,10 @@ import { MainComponent } from './main/main.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { OrdersComponent } from './components/orders/orders.component';
+import { NewsComponent } from './components/news/news.component';
+import { NewsDetailComponent } from './components/news-detail/news-detail.component';
+import { OrdersDetailComponent } from './components/orders-detail/orders-detail.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 
 
 const routes: Routes = [
@@ -14,7 +18,12 @@ const routes: Routes = [
       { path: 'dashboard-main', component: OrdersComponent},
       { path: 'product-list', component: ProductListComponent},
       { path: 'product-detail', component: ProductDetailComponent},
-      { path: 'user-list', component: UserListComponent}
+      { path: 'user-list', component: UserListComponent},
+      { path: 'news-list', component: NewsComponent},
+      { path: 'news-detail', component: NewsDetailComponent},
+      { path: 'order-detail', component: OrdersDetailComponent},
+      { path: 'user-detail', component: UserDetailComponent},
+
   ]}
 ];
 
